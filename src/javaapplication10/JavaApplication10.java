676a05427e5c1b8e548bbf8dec8329a0013de390
18763 <18763@1k122s7.dune.edu>
2021-04-07 11:55:19 +0200

    initial commit

--- src/javaapplication10/JavaApplication10.java
+++ src/javaapplication10/JavaApplication10.java
@@ -5,17 +5,31 @@
  */
 package javaapplication10;
 
-/**
- *
- * @author 18763
- */
+import hiber.entity.Studentyi;
+import Hiber.NewHibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Query;
+
+
 public class JavaApplication10 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+      SessionFactory sf = NewHibernateUtil.getSessionFactory();
+        Session s = sf.openSession();
+        String hql = "FROM Studentyi";
+       // String hql = "FROM Gruppyi WHERE nazvanie like 'A-%' ";
+        Query query =  s.createQuery(hql);
+        List <Studentyi> students = query.list();
+       
+        /* for(int i = 0; i < students.size(); i++){
+        System.out.println(students.get(i).getFamiliya() +"\t " +students.get(i).getImya() 
+                            +" " + students.get(i).getOtchestvo() +"\t " + students.get(i).getGruppyi().getNazvanie());
+        }*/
     }
     
 }
